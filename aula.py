4cb6833d26ed90871105578e8bb70458e4a2d078
DINITHA <dinitha.9@gmail.com>
2024-02-19 12:05:08 -0300

    Create aula.py

---
+++ aula.py
@@ -0,0 +1,86 @@
+#print("Olá, mundo!")
+
+#nome = input("Digite seu nome: ")
+#print("Olá,", nome, "! Seja bem-vindo(a)!")
+
+
+#operador + para concatenar strings em vez de usar vírgulas
+#nome = input("Digite seu nome: ")
+#print("Olá, " + nome + "! Seja bem-vindo(a)!")
+
+
+# Saída usando concatenação de strings
+#curso = "Python"
+#print("Estou aprendendo " + curso)
+
+
+# Definindo variáveis do tipo inteiro
+#idade = 25
+#quantidade_produtos = 10
+#numero_negativo = -7
+
+#nome = "João"  # Uma variável do tipo string
+#idade = 25  # Uma variável do tipo inteiro
+#altura = 1.75  # Uma variável do tipo float
+
+#idade = 25
+#print("Minha idade é:", idade)
+ano_nascimento = int(input("Digite o ano do seu nascimento: "))
+
+# Obtendo o ano atual (pode variar dependendo de como você está executando o código)
+ano_atual = 2024
+
+# Calculando a idade
+idade = ano_atual - ano_nascimento
+
+print("Sua idade é:", idade)
+
+
+
+
+
+## Realizando operações matemáticas 
+#soma = 5 + 3
+#subtracao = 10 - 4
+#multiplicacao = 6 * 5
+#divisao = 15 / 3
+#resto_divisao = 17 % 4
+#potencia = 2 ** 3
+
+
+## Exemplos de expressões com inteiros
+#maior = idade > 18
+#igual = quantidade_produtos == 0
+
+
+
+## Conversão 
+#numero_texto = "10"
+#numero_inteiro = int(numero_inteiro)
+#numero_float = float(numero_float)
+#booleano = int(booleano)
+
+
+#ex:
+#converter um número float para inteiro
+#numero_float = 3.14
+#numero_inteiro = int(numero_float)
+#print(numero_inteiro)  # Saída: 3
+
+#converter um número inteiro para float
+#numero_inteiro = 10
+#numero_float = float(numero_inteiro)
+#print(numero_float)  # Saída: 10.0
+
+#import math
+
+#raio = float(input("Digite o raio do círculo: "))
+#
+#area = math.pi * raio**2
+#
+#print("A área do círculo é:", area)
+
+
+
+
+
